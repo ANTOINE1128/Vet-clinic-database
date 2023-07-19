886efd9c2b2d42dc8ff921bc8b6c346118bfdd9f
ANTOINE1128 <makdessyantoine@gmail.com>
2023-07-19 12:49:04 +0300

    add queries

--- queries.sql
+++ queries.sql
@@ -9,6 +9,10 @@ SELECT * FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE name Not LIKE '%mon';
+COMMIT;
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
