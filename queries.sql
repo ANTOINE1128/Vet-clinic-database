851c5d4358bcd153073193bcb7f8a17bcb01301b
ANTOINE1128 <makdessyantoine@gmail.com>
2023-07-19 18:15:04 +0300

    update queries

--- queries.sql
+++ queries.sql
@@ -9,6 +9,12 @@ SELECT * FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
