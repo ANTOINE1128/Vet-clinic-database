42c8edb1bcff7781d18740123463afee912bf2b2
ANTOINE1128 <makdessyantoine@gmail.com>
2023-07-20 13:23:05 +0300

    add tables

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
- id SERIAL PRIMARY KEY,
+ id SERIAL PRIMARY KEY, 
   name VARCHAR(255),
   date_of_birth DATE,
   escape_attempts INTEGER,
@@ -9,3 +9,24 @@ CREATE TABLE animals (
   weight_kg DECIMAL(10, 2)
 );
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER
+);
+--  id is already set for the animals table
+ALTER TABLE animals
+DROP COLUMN species;
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species(id);
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species(id);
+ALTER TABLE owners
+ADD COLUMN owners_id INTEGER REFERENCES owners(id);
