92afcf62061adbb1d543786a80e12ee31f3cabe3
ANTOINE1128 <makdessyantoine@gmail.com>
2023-07-21 14:08:11 +0300

    delete a querie

--- schema.sql
+++ schema.sql
@@ -26,7 +26,5 @@ ALTER TABLE animals
 DROP COLUMN species;
 ALTER TABLE animals
 ADD COLUMN species_id INTEGER REFERENCES species(id);
-ALTER TABLE animals
-ADD COLUMN species_id INTEGER REFERENCES species(id);
 ALTER TABLE owners
 ADD COLUMN owners_id INTEGER REFERENCES owners(id);
